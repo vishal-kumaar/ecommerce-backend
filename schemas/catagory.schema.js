325118bb5568cc60dbe24a7ca8faa0d6fb4fd7a0
Vishal Kumar <vishal.kumar08376@gmail.com>
2024-01-23 19:20:10 +0530

    Create - Catagory schema

---
+++ schemas/catagory.schema.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Category description is required"],
+    },
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Category", categorySchema);
