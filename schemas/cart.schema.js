735981c8b4e075c1f073dd34496b174e04d22682
Vishal Kumar <vishal.kumar08376@gmail.com>
2024-01-23 19:08:14 +0530

    Create - Cart schema

---
+++ schemas/cart.schema.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+    totalAmount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Cart", cartSchema);
